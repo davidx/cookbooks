86ce0fd0e299a06526c7123d9ab009fa89808dc0
David Andersen <davidx@gmail.com>
2011-01-09 00:45:06 -0800

    add clusters

--- ganglia/recipes/client.rb
+++ ganglia/recipes/client.rb
@@ -2,11 +2,11 @@
 portage_package "sys-cluster/ganglia" do
   action :install
 end
-service "gmond" do
-  action [:enable, :start]
-  supports :status => true, :start => true, :stop => true, :restart => true, :reload => true
-end
 template "/etc/ganglia/gmond.conf" do
   source "gmond.conf.erb"
   notifies :restart, "service[gmond]"
+end
+service "gmond" do
+  action [:enable, :start]
+  supports :status => true, :start => true, :stop => true, :restart => true
 end

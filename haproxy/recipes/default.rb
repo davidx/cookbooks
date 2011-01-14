239e90755ab640b68eeedeb40b923c68c26a1b90
David Andersen <davidx@gmail.com>
2011-01-13 21:36:52 -0800

    first-time run state order

--- haproxy/recipes/default.rb
+++ haproxy/recipes/default.rb
@@ -2,12 +2,12 @@ portage_package "haproxy" do
   action :install
 end
 
-service "haproxy" do
-  action [:enable, :start]
-  supports :status => true, :start => true, :stop => true, :restart => true, :reload => true
-end
-
 template "/etc/haproxy.cfg" do
   source "haproxy.cfg.erb"
   notifies :reload, "service[haproxy]"
+end
+
+service "haproxy" do
+  action [:enable, :start]
+  supports :status => true, :start => true, :stop => true, :restart => true, :reload => true
 end

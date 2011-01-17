de0be34655b11d5a5c0202e6b1c39ca9756a88b0
David Andersen <davidx@gmail.com>
2011-01-16 23:31:52 -0800

    no execute block needed

--- clip/recipes/AC-11.rb
+++ clip/recipes/AC-11.rb
@@ -3,15 +3,15 @@
 
 # AC-11(1)
 # Kickstart Actions:
-execute "AC-11" do
+
   ## (GEN000500: CAT II) (Previously - G605) The SA will configure systems to log
   ## out interactive processes (i.e., terminal sessions, ssh sessions, etc.,)
   ## after 15 minutes of inactivity or ensure a password protected screen lock
   ## mechanism is used and is set to lock the screen after 15 minutes of
   ## inactivity.
 
-  edit_file "/etc/profile/timeout" do
-    append_if_no_such_line "readonly TMOUT=900"
-  end
+edit_file "/etc/profile/timeout" do
+  append_if_no_such_line "readonly TMOUT=900"
 end
+
 
